ef384e7449453ac595ebf38b2c385af63e007698
EUGENIA56 <edzadu6@gmail.com>
2023-07-12 09:24:26 +0000

    break

--- main.py
+++ main.py
@@ -2,12 +2,29 @@
 import random
 
 # correct guess number
-guess_number = 9
-user_input = int(input('Enter guess a number(1-20): '))
-if user_input == guess_number:
-    print('You guess correct')
-else:
-    print('Guess is wrong')
 
-# abc = random.randint(1, 10)
-# print(abc)
+i=0
+while i<=5:
+    guess_number = random.randint(1, 20)
+    user_input = int(input('Enter guess a number(1-20): '))
+    if user_input == guess_number:
+        print('You guess correct')
+        break
+    else:
+        print('Guess is wrong')
+    i=i+1
+
+abc = random.randint(1, 10)
+print(abc)
+
+# break
+# for i in range(10):
+#     if i == 2:
+#         break
+#     print('Hello World')
+
+# continue                                                                        
+# for i in range(10):
+#     if i == 2:
+#         continue
+#     print(i)
