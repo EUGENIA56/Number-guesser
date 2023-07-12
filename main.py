38435ca79223e416d987565a2eb20b40fc8cb76f
EUGENIA56 <edzadu6@gmail.com>
2023-07-12 09:53:14 +0000

    number of chances

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import random
 # correct guess number
 
 i=0
+number_of_chances=0
 while i<5:
     guess_number = random.randint(1, 20)
     user_input = int(input('Enter guess a number(1-20): '))
+    number_of_chances+=1
     if user_input == guess_number:
         print('You guess correct')
+        res = 'You won and you used {} chances'
+        print(res.format(number_of_chances))
         break
     else:
         print('Guess is wrong')
@@ -17,6 +21,7 @@ while i<5:
         else:
             print('Your guesss is too low, try again!!')
     i=i+1
+    
 
 abc = random.randint(1, 10)
 print(abc)
