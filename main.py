04a7723ed82e626cc501cf0953732ce2ceb42d7a
EUGENIA56 <edzadu6@gmail.com>
2023-07-12 09:39:06 +0000

    user_input > guess_number

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 # correct guess number
 
 i=0
-while i<=5:
+while i<5:
     guess_number = random.randint(1, 20)
     user_input = int(input('Enter guess a number(1-20): '))
     if user_input == guess_number:
@@ -12,6 +12,10 @@ while i<=5:
         break
     else:
         print('Guess is wrong')
+        if user_input > guess_number:
+            print('Your number is too high, try again!!')
+        else:
+            print('Your guesss is too low, try again!!')
     i=i+1
 
 abc = random.randint(1, 10)
